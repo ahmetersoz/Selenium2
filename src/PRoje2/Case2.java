ff483dcf8c1fc8b8742b531d0ab1d12d93227e34
ahmet <ahmetersz@gmail.com>
2023-03-10 20:09:09 +0300

    case2 ok

--- src/PRoje2/Case2.java
+++ src/PRoje2/Case2.java
@@ -1,6 +1,7 @@
 package PRoje2;
 
 import Utlity.BaseDriver;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -12,6 +13,10 @@ import java.time.Duration;
 public class Case2 extends BaseDriver {
     @Test
     public void Case2(){
+
+        driver.get("https://shopdemo.e-junkie.com/");
+
+
         WebElement ebook = driver.findElement(By.cssSelector("button[onclick*='1595015']"));
         ebook.click();
 
@@ -20,6 +25,15 @@ public class Case2 extends BaseDriver {
         WebDriverWait waitbutton = new WebDriverWait(driver, Duration.ofSeconds(5));
 
         waitbutton.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frame));
+
+            WebElement debit=driver.findElement(By.xpath("//*[@data-option='CC']"));
+            debit.click();
+            WebElement payBut=driver.findElement(By.cssSelector("[class='Pay-Button']"));
+            payBut.click();
+            WebElement warning=driver.findElement(By.cssSelector("[name='close-circle']+span"));
+            waitbutton.until(ExpectedConditions.visibilityOf(warning));
+        Assert.assertTrue("Mesaj Görüntülenemedi",warning.isDisplayed());
+        BekleKapat();
 
     }
 }
